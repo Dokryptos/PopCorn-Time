d5bb328eaa2acfcdc0433c0cb60f196201d841de
Dok <pierredok@outlook.fr>
2023-06-20 12:51:25 +0200

    Setup React

---
+++ src/components/Footer.js
@@ -0,0 +1,4 @@
+function Footer(props){
+    return <h1>This is my Footer</h1>
+}
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,4 @@
+function Main(props){
+    return <h1>This is my Main</h1>
+}
+export default Main
